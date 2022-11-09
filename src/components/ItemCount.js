7d9a6e7476dc8a7686c483c0e8b1e54bf0321c21
Ivan Gutierrez <ingutierrez@uc.cl>
2022-11-09 15:20:17 -0300

    Item Count funcional, salvo el boton de añadir

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { useState } from "react";
 
 const ItemCount = () => {
-    return (<div></div>)
+
+    const [contador, setContador] = useState(1);
+
+
+    const handleSuma = () => {
+        setContador(contador + 1);
+    }
+
+    const handleResta = () => {
+        if (contador > 0) {
+            setContador(contador - 1);
+        }
+
+    }
+
+    const añadirCarrito = () => {
+        console.log(contador + " productos añadidos")
+    }
+    return (
+        <div>
+            <nav class="itemCount">
+                <button onClick={handleSuma} class="botonItem">+</button>
+                <p>{contador}</p>
+                <button onClick={handleResta} class="botonItem">-</button>
+            </nav>
+            <button onClick={añadirCarrito}>Añadir al carrito</button>
+        </div>)
 }
 
 export default ItemCount
--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -2,14 +2,12 @@ import React from "react";
 import ItemCount from "./ItemCount";
 
 const ItemDetail = ({producto}) => {
-    console.log(producto)
     return (
         <div>
             <nav class="detailProduct">
                 <h1>{producto[0].nombre}</h1>
-                <img src={producto[0].url} height="400"></img>
+                <img src={producto[0].url} height="400" alt=""></img>
                 <h2>{producto[0].precio} CLP</h2>
-                <button>Añadir al carrito</button>
                 <ItemCount />
             </nav>
         </div>
